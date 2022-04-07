532920a2a3d7f9ecd2788327ed94371255dca338
Ander <91115165+anderlss04@users.noreply.github.com>
2022-04-07 12:29:49 +0200

    actualizacion perfil

--- web_ander_proyecto/src/app/app.component.ts
+++ web_ander_proyecto/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import  {  faTruckFast, faUserCircle, faPaperPlane, faPeopleGroup  }  from  '@fortawesome/free-solid-svg-icons' ;
 
 
 @Component({
@@ -10,5 +11,10 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent {
-  title = 'web_ander_proyecto';
+
+  faUserCircle = faUserCircle;
+  faPaperPlane = faPaperPlane;
+  faPeopleGroup = faPeopleGroup;
+  faTruckFast = faTruckFast;
+ 
 }
